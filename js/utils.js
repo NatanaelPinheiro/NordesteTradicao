adddde8d4632397318ac15a861fc935e1e5d248c
Natanael Pinheiro <natanael.pinheiro@futuraproducoes.com.br>
2023-09-07 20:05:48 -0300

    trabalhando no header mobile

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,11 @@
+const navMobile = document.querySelector('.navbar-mobile');
 function toggleMenu(){
-    const navMobile = document.querySelector('.navbar-mobile');
     navMobile.classList.toggle('active');
 }
+
+const navMobileLinks = document.querySelectorAll('.navbar-mobile--list--item a');
+navMobileLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navMobile.classList.remove('active');
+    })
+});
